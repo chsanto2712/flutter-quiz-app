76af5facbfaa6f71e9fbd4fdf22a229cd999d13f
chsanto2712 <ramsrisantosh2712@gmail.com>
2023-09-30 17:57:30 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
 
-// import 'package:quiz_app/quiz.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: Text('Hello World'),
-      ),
-    ),
+    const Quiz(),
   );
 }
